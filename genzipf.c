c3f6f64ad3918bd45ab575503a5f45191c419dba
Vignesh <vignesh.r@cs.rutgers.edu>
2012-11-03 17:32:13 -0400

    first commit

---
+++ genzipf.c
@@ -0,0 +1,146 @@
+//==================================================== file = genzipf.c =====
+//=  Program to generate Zipf (power law) distributed random variables      =
+//===========================================================================
+//=  Notes: 1) Writes to a user specified output file                       =
+//=         2) Generates user specified number of values                    =
+//=         3) Run times is same as an empirical distribution generator     =
+//=         4) Implements p(i) = C/i^alpha for i = 1 to N where C is the    =
+//=            normalization constant (i.e., sum of p(i) = 1).              =
+
+//=  History: KJC (11/16/03) - Genesis (from genexp.c)                      =
+//===========================================================================
+//----- Include files -------------------------------------------------------
+#include <assert.h>             // Needed for assert() macro
+#include <stdio.h>              // Needed for printf()
+#include <stdlib.h>             // Needed for exit() and ato*()
+#include <math.h>
+
+//----- Constants -----------------------------------------------------------
+#define  FALSE          0       // Boolean false
+#define  TRUE           1       // Boolean true
+
+//----- Function prototypes -------------------------------------------------
+int      zipf(double alpha, int n);  // Returns a Zipf random variable
+double   rand_val(int seed);         // Jain's RNG
+
+//  double alpha;                 // Alpha parameter
+//  double n;                     // N parameter
+//  int    num_values;            // Number of values
+//  int    zipf_rv;               // Zipf random variable
+//  int    i;                     // Loop counter
+//===== Zipf Value Generator ========================================================
+int*  generate_zipf(int num_values,double alpha, double n,int seed)
+{
+  int* ret_val; //return values to be returned to the caller.
+   int zipf_rv;
+  int i;
+  ret_val=malloc(sizeof(int)*num_values);
+  rand_val(seed);
+
+
+  // Generate and output zipf random variables
+  for (i=0; i<num_values; i++)
+  {
+    zipf_rv = zipf(alpha, n);
+
+
+	ret_val[i]=zipf_rv;
+  }
+zipf(1500,0);
+
+return ret_val;
+}
+
+
+//===========================================================================
+//=  Function to generate Zipf (power law) distributed random variables     =
+//=    - Input: alpha and N                                                 =
+//=    - Output: Returns with Zipf distributed random variable              =
+//===========================================================================
+int zipf(double alpha, int n)
+{
+  static int first = TRUE;      // Static first time flag
+  static double c = 0;          // Normalization constant
+  double z;                     // Uniform random number (0 < z < 1)
+  double sum_prob;              // Sum of probabilities
+  double zipf_value=0.0;            // Computed exponential value to be returned
+  int    i;                     // Loop counter
+
+  // Compute normalization constant on first call only
+  if (first == TRUE)
+  {
+    for (i=1; i<=n; i++)
+      c = c + (1.0 / pow((double) i, alpha));
+    c = 1.0 / c;
+    first = FALSE;
+  }
+
+   if(alpha==1500)
+   {
+       first=TRUE;
+       return 0;
+   }
+
+  // Pull a uniform random number (0 < z < 1)
+  do
+  {
+    z = rand_val(0);
+  }
+  while ((z == 0) || (z == 1));
+
+  // Map z to the value
+  sum_prob = 0;
+  for (i=1; i<=n; i++)
+  {
+    sum_prob = sum_prob + c / pow((double) i, alpha);
+    if (sum_prob >= z)
+    {
+      zipf_value = i;
+      break;
+    }
+  }
+
+  // Assert that zipf_value is between 1 and N
+  assert((zipf_value >=1) && (zipf_value <= n));
+
+  return(zipf_value);
+}
+
+
+//=========================================================================
+//= Multiplicative LCG for generating uniform(0.0, 1.0) random numbers    =
+//=   - x_n = 7^5*x_(n-1)mod(2^31 - 1)                                    =
+//=   - With x seeded to 1 the 10000th x value should be 1043618065       =
+//=   - From R. Jain, "The Art of Computer Systems Performance Analysis," =
+//=     John Wiley & Sons, 1991. (Page 443, Figure 26.2)                  =
+//=========================================================================
+double rand_val(int seed)
+{
+  const long  a =      16807;  // Multiplier
+  const long  m = 2147483647;  // Modulus
+  const long  q =     127773;  // m div a
+  const long  r =       2836;  // m mod a
+  static long x;               // Random int value
+  long        x_div_q;         // x divided by q
+  long        x_mod_q;         // x modulo q
+  long        x_new;           // New x value
+
+  // Set the seed if argument is non-zero and then return zero
+  if (seed > 0)
+  {
+    x = seed;
+    return(0.0);
+  }
+
+  // RNG using integer arithmetic
+  x_div_q = x / q;
+  x_mod_q = x % q;
+  x_new = (a * x_mod_q) - (r * x_div_q);
+  if (x_new > 0)
+    x = x_new;
+  else
+    x = x_new + m;
+
+  // Return a random value between 0.0 and 1.0
+  return((double) x / m);
+}
